78a829f56a47f5bf42e04643e947559aff565aff
jam <jamilenetan23@gmail.com>
2024-06-05 15:52:52 +0800

    front end

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,7 +10,7 @@ class RegisterController extends Controller
 {
     public function showRegistrationForm()
     {
-        return view('auth.register');
+        return view('register');
     }
 
     public function register(Request $request)
--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Registration routes (accessible to unauthenticated users)
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+// Login routes (accessible to unauthenticated users)
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+
 Route::middleware(['auth'])->group(function () {
+    // Routes accessible only to authenticated users
     Route::resource('subjects', SubjectController::class);
     Route::resource('enrollments', EnrollmentController::class);
 
     Route::get('subjects/enrollments', [SubjectController::class, 'viewEnrollments'])->name('subjects.enrollments');
-
-    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-    Route::post('/register', [RegisterController::class, 'register']);
-
-    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-    Route::post('/login', [LoginController::class, 'login']);
-
 });
