ef712ce443985001a6581f40b2f961cda64c7f79
Marxel Abogado <xyldxal@gmail.com>
2024-06-05 15:17:52 +0800

    login and register controllers with routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\EnrollmentController;
+
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,5 +17,11 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('enrollments', EnrollmentController::class);
 
     Route::get('subjects/enrollments', [SubjectController::class, 'viewEnrollments'])->name('subjects.enrollments');
+
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
 
 });
