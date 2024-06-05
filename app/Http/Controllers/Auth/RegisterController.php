1d3ea85adb0de14a5cad96ac6c10592d09e4cd71
Marxel Abogado <xyldxal@gmail.com>
2024-06-05 16:29:11 +0800

    edit register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -11,7 +11,7 @@ class RegisterController extends Controller
 {
     public function showRegistrationForm()
     {
-        return view('register');
+        return view('auth.register');
     }
 
     public function register(Request $request)
