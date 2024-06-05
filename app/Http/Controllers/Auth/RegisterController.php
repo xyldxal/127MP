f0a382b9431db2e55327e7c37e483abc8725d2de
Marxel Abogado <xyldxal@gmail.com>
2024-06-05 16:21:21 +0800

    register user import

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -13,42 +14,22 @@ class RegisterController extends Controller
         return view('register');
     }
 
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'role' => ['required', 'string', 'in:student,professor'],
-        ]);
-    }
-
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-            'role' => $data['role'],
-        ]);
-    }
-
     public function register(Request $request)
     {
-        $this->validator($request->all())->validate();
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+            'role' => 'required|string|in:student,professor', // Validate role
+        ]);
 
-        $user = $this->create($request->all());
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role, // Assign role
+        ]);
 
-        // Automatically log in the user after registration
-        auth()->login($user);
-
-        return redirect($this->redirectPath());
+        return redirect()->route('login');
     }
-
-    protected function redirectPath()
-    {
-        return $this->redirectTo;
-    }
-
-
 }
