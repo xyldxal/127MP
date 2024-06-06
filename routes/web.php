9d238959b0c103c07a0525fe8a6cd359c6056bbf
Marxel Abogado <xyldxal@gmail.com>
2024-06-06 17:10:35 +0800

    change landing page route to login

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 // Registration routes (accessible to unauthenticated users)
