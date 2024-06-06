f0179158c7adc437e93bc1f799526501a05daec5
jam <jamilenetan23@gmail.com>
2024-06-06 10:56:01 +0800

    dash

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::post('/register', [RegisterController::class, 'register']);
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
+// Logout route
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
