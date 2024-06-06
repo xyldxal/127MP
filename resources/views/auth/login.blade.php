cd7934d015bad32abc060b60753b0c88f4e59983
Marxel Abogado <xyldxal@gmail.com>
2024-06-06 10:01:26 +0800

    initial login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,43 +1,26 @@
-<!-- resources/views/auth/login.blade.php -->
-
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <h1>Login</h1>
-
+<!DOCTYPE html>
+<html lang="en">
+<body>
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <div>
             <label for="email">Email</label>
             <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
-            @error('email')
-                <span>{{ $message }}</span>
-            @enderror
         </div>
 
         <div>
             <label for="password">Password</label>
             <input id="password" type="password" name="password" required autocomplete="current-password">
-            @error('password')
-                <span>{{ $message }}</span>
-            @enderror
+        </div>
+
+        <div>
+            <input type="checkbox" name="remember" id="remember">
+            <label for="remember">Remember Me</label>
         </div>
 
         <div>
             <button type="submit">Login</button>
         </div>
-
-        @if ($errors->any())
-            <div>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
     </form>
-</div>
-@endsection
+</body>
