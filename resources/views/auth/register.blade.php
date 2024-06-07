ba1da8e9ade1e7d354893f39aa3ba53dd4d7ab0c
James <jamesardc19@gmail.com>
2024-06-08 01:31:10 +0800

    front end

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UPM Registration</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/registration.css') }}" >
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+</head>
 <body>
     <nav>
-        <a href="/">Home</a>
         <a href="/login">Login</a>
     </nav>
     <div class="container">
@@ -20,10 +19,21 @@
             <input type="text" id="name" name="name" placeholder="Your Name Here" required>
             <label for="email">Email:</label>
             <input id="email" type="email" name="email" placeholder="Your Email Here" value="{{ old('email') }}" required>
+
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"required>
+            <div class="checkbox-container">
+                <input type="checkbox" id="show-password">
+                <label for="show-password">Show Password</label>
+            </div>
+
             <label for="password_confirmation">Confirm Password:</label>
             <input type="password" id="password_confirmation" name="password_confirmation" required>
+            <div class="checkbox-container">
+                <input type="checkbox" id="show-password-confirm">
+                <label for="show-password-confirm">Show Password</label>
+            </div>
+
             <label for="role">Role:</label>
             <select id="role" name="role" required>
                 <option value="student">Student</option>
@@ -32,6 +42,25 @@
             <button type="submit">Register</button>
         </form>
     </div>
-</body>
 
+    <script>
+        document.getElementById('show-password').onclick = function() {
+            var password = document.getElementById('password');
+            if (password.type === "password") {
+                password.type = "text";
+            } else {
+                password.type = "password";
+            }
+        };
+
+        document.getElementById('show-password-confirm').onclick = function() {
+            var passwordConfirm = document.getElementById('password_confirmation');
+            if (passwordConfirm.type === "password") {
+                passwordConfirm.type = "text";
+            } else {
+                passwordConfirm.type = "password";
+            }
+        };
+    </script>
+</body>
 </html>
